b94153547cd17231de0b50b80997bc2af64a3dcb
AlanAOliveira <alanalves123456789@gmail.com>
2024-04-15 12:50:14 -0300

    1.0
    primeiro commit so tem os arquivos iniciais

---
+++ main.js
@@ -0,0 +1,10 @@
+function calculo(){
+    console.log("calculo")
+    let hp1 = document.getElementById("HP") 
+    let dano1 = document.getElementById("DANO")
+    let hp2 = document.getElementById("HP2")
+    let dano2 = document.getElementById("DANO2")
+
+    let x1 = {"HP":hp1,"DANO":dano1}
+
+}
